9069898ff10b55c26b4828ca6e7383be1fc8cb40
Ayanna Scoon-Jerome <ayanna.scoon@gmail.com>
2018-02-08 15:59:03 -0500

    created directory structure, and added necessary files

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers 
+	(burger_name, devoured)
+VALUES 
+	("Shake yo' tail feather", false),
+    ("Spicy Garlic", true),
+    ("Mediterranean madness", false),
+    ("Zinger", true);
